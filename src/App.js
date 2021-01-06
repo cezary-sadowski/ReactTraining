12473ab1c62232a1f3920e985465a17ac2158e02
Cezary <cezary.sadowski@asseco.pl>
2021-01-06 17:27:45 +0100

    30 Understanding JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I'm inside h1</h1>
+      </div>
+    );
+
+    //Other way but cumbersome:
+    // return React.createElement('div', {className: 'App'}, 
+    //   React.createElement('h1', null, ' I\'m inside h1'));
+  }
 }
 
 export default App;
