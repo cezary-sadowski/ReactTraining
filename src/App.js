909307a22912956455c9aca7e8cafe885a98bf99
Cezary <cezary.sadowski@asseco.pl>
2021-01-06 18:10:14 +0100

    32. Creating a Functional Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
+import Person from './Person/Person.js'
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm inside h1</h1>
         <p>Same here!</p>
+        <Person />
       </div>
     );
 
