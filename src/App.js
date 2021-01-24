d7c0bf4688d7f5471c03faae6f23cfc5c674db4d
Cezary <cezary.sadowski@asseco.pl>
2021-01-24 15:56:23 +0100

    44. Using the useState() Hook. WITH COMMENTS, IMPORTANT! (functional component)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person.js'
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [peopleState, setPeopleState] = useState({
     persons: [
       { name: 'Cezary', age: 26 },
       { name: 'Grzes', age: 40 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    //dont change state directly like state.person[0].name = '...' ! it doesnt work and its not good at all.
+  // Array destructuring - useState() returns an array with exactly two elemenents:
+  // 1. My current state value
+  // 2. A method to update my state value
+  // 'peopleState' could be any data of my choice (obj, array, string etc);
 
-    this.setState({
+
+  const [otherState, setOtherState] = useState({otherStateXD: 'other state :D'});
+  // Its a good practice to use useState calls more times bcs actually its not merging other values together
+  // for example if my first useState has sth more than persons object I have to remember to pass it too in switchNameHandler 
+  // during manipulating setPeopleState (bcs it will be lost);
+
+  console.log(peopleState, otherState);
+
+  const switchNameHandler = () => {
+    setPeopleState({
       persons: [
         { name: 'Cezary', age: 27 },
         { name: 'Grzes', age: 42 }
-      ]
+      ],
     })
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Im children props</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={peopleState.persons[0].name} age={peopleState.persons[0].age}>Im children props</Person>
+      <Person name={peopleState.persons[1].name} age={peopleState.persons[1].age} />
+    </div>
+  );
+
+  // switchNameHandler without '()' bcs I only pass a reference to the method;
 }
 
 export default App;
